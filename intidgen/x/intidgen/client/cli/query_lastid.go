058ffc5571eadea06ddb8c459e24f97ec5995454
xmonader <xmonader@gmail.com>
2022-02-23 11:26:40 +0200

    lastid query

---
+++ intidgen/x/intidgen/client/cli/query_lastid.go
@@ -0,0 +1,42 @@
+package cli
+
+import (
+	"strconv"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/spf13/cobra"
+	"github.com/xmonader/intidgen/x/intidgen/types"
+)
+
+var _ = strconv.Itoa(0)
+
+func CmdLastid() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "lastid",
+		Short: "Query lastid",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) (err error) {
+
+			clientCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			queryClient := types.NewQueryClient(clientCtx)
+
+			params := &types.QueryLastidRequest{}
+
+			res, err := queryClient.Lastid(cmd.Context(), params)
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
+}
--- intidgen/x/intidgen/types/query.pb.go
+++ intidgen/x/intidgen/types/query.pb.go
@@ -113,34 +113,119 @@ func (m *QueryParamsResponse) GetParams() Params {
 	return Params{}
 }
 
+type QueryLastidRequest struct {
+}
+
+func (m *QueryLastidRequest) Reset()         { *m = QueryLastidRequest{} }
+func (m *QueryLastidRequest) String() string { return proto.CompactTextString(m) }
+func (*QueryLastidRequest) ProtoMessage()    {}
+func (*QueryLastidRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_55f3aba7d804c932, []int{2}
+}
+func (m *QueryLastidRequest) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryLastidRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryLastidRequest.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryLastidRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryLastidRequest.Merge(m, src)
+}
+func (m *QueryLastidRequest) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryLastidRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryLastidRequest.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryLastidRequest proto.InternalMessageInfo
+
+type QueryLastidResponse struct {
+	Lastid string `protobuf:"bytes,1,opt,name=lastid,proto3" json:"lastid,omitempty"`
+}
+
+func (m *QueryLastidResponse) Reset()         { *m = QueryLastidResponse{} }
+func (m *QueryLastidResponse) String() string { return proto.CompactTextString(m) }
+func (*QueryLastidResponse) ProtoMessage()    {}
+func (*QueryLastidResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_55f3aba7d804c932, []int{3}
+}
+func (m *QueryLastidResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *QueryLastidResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_QueryLastidResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *QueryLastidResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_QueryLastidResponse.Merge(m, src)
+}
+func (m *QueryLastidResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *QueryLastidResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_QueryLastidResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_QueryLastidResponse proto.InternalMessageInfo
+
+func (m *QueryLastidResponse) GetLastid() string {
+	if m != nil {
+		return m.Lastid
+	}
+	return ""
+}
+
 func init() {
 	proto.RegisterType((*QueryParamsRequest)(nil), "xmonader.intidgen.intidgen.QueryParamsRequest")
 	proto.RegisterType((*QueryParamsResponse)(nil), "xmonader.intidgen.intidgen.QueryParamsResponse")
+	proto.RegisterType((*QueryLastidRequest)(nil), "xmonader.intidgen.intidgen.QueryLastidRequest")
+	proto.RegisterType((*QueryLastidResponse)(nil), "xmonader.intidgen.intidgen.QueryLastidResponse")
 }
 
 func init() { proto.RegisterFile("intidgen/query.proto", fileDescriptor_55f3aba7d804c932) }
 
 var fileDescriptor_55f3aba7d804c932 = []byte{
-	// 301 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x90, 0xcd, 0x4a, 0x03, 0x31,
-	0x14, 0x85, 0x27, 0xa2, 0x5d, 0xc4, 0x5d, 0xac, 0x20, 0x83, 0x44, 0x19, 0x5c, 0x48, 0xc1, 0x84,
-	0xd6, 0x17, 0x90, 0x6e, 0xdc, 0x6a, 0x37, 0x82, 0xbb, 0x4c, 0x1b, 0x62, 0xc0, 0xc9, 0x4d, 0x27,
-	0xa9, 0xb4, 0x5b, 0x9f, 0x40, 0x10, 0xd7, 0xbe, 0x4e, 0x97, 0x05, 0x37, 0xae, 0x44, 0x66, 0x7c,
-	0x10, 0xe9, 0x64, 0x3a, 0xe2, 0x2f, 0xee, 0x2e, 0xf7, 0x9e, 0xef, 0xe4, 0xe4, 0xe0, 0xb6, 0x36,
-	0x5e, 0x8f, 0x94, 0x34, 0x7c, 0x3c, 0x91, 0xf9, 0x8c, 0xd9, 0x1c, 0x3c, 0x90, 0x78, 0x9a, 0x81,
-	0x11, 0x23, 0x99, 0xb3, 0xd5, 0xb9, 0x19, 0xe2, 0xb6, 0x02, 0x05, 0x95, 0x8c, 0x2f, 0xa7, 0x40,
-	0xc4, 0xbb, 0x0a, 0x40, 0x5d, 0x4b, 0x2e, 0xac, 0xe6, 0xc2, 0x18, 0xf0, 0xc2, 0x6b, 0x30, 0xae,
-	0xbe, 0x76, 0x86, 0xe0, 0x32, 0x70, 0x3c, 0x15, 0x4e, 0x86, 0x87, 0xf8, 0x4d, 0x37, 0x95, 0x5e,
-	0x74, 0xb9, 0x15, 0x4a, 0x9b, 0x4a, 0x5c, 0x6b, 0xb7, 0x9b, 0x44, 0x56, 0xe4, 0x22, 0xab, 0x2d,
-	0x92, 0x36, 0x26, 0xe7, 0x4b, 0xf0, 0xac, 0x5a, 0x0e, 0xe4, 0x78, 0x22, 0x9d, 0x4f, 0x2e, 0xf0,
-	0xd6, 0xa7, 0xad, 0xb3, 0x60, 0x9c, 0x24, 0x27, 0xb8, 0x15, 0xe0, 0x1d, 0xb4, 0x8f, 0x0e, 0x37,
-	0x7b, 0x09, 0xfb, 0xfd, 0x43, 0x2c, 0xb0, 0xfd, 0xf5, 0xf9, 0xcb, 0x5e, 0x34, 0xa8, 0xb9, 0xde,
-	0x23, 0xc2, 0x1b, 0x95, 0x33, 0x79, 0x40, 0xb8, 0x15, 0x24, 0x84, 0xfd, 0x65, 0xf3, 0x3d, 0x5d,
-	0xcc, 0xff, 0xad, 0x0f, 0xb9, 0x93, 0xce, 0xed, 0xd3, 0xdb, 0xfd, 0xda, 0x01, 0x49, 0xf8, 0x0a,
-	0xe4, 0x4d, 0x1b, 0x5f, 0x6a, 0xe9, 0x9f, 0xce, 0x0b, 0x8a, 0x16, 0x05, 0x45, 0xaf, 0x05, 0x45,
-	0x77, 0x25, 0x8d, 0x16, 0x25, 0x8d, 0x9e, 0x4b, 0x1a, 0x5d, 0x1e, 0x29, 0xed, 0xaf, 0x26, 0x29,
-	0x1b, 0x42, 0xf6, 0x83, 0xcf, 0xf4, 0x63, 0xf4, 0x33, 0x2b, 0x5d, 0xda, 0xaa, 0x0a, 0x3e, 0x7e,
-	0x0f, 0x00, 0x00, 0xff, 0xff, 0xcf, 0x0a, 0x2d, 0x9f, 0x0b, 0x02, 0x00, 0x00,
+	// 347 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x52, 0xcd, 0x4a, 0xc3, 0x40,
+	0x18, 0x4c, 0x8a, 0x06, 0x5c, 0x6f, 0xb1, 0x8a, 0x04, 0x89, 0x12, 0x3c, 0x48, 0xa1, 0x59, 0x5a,
+	0x5f, 0x40, 0x7a, 0xf1, 0xe2, 0x41, 0x7b, 0x11, 0xbc, 0x6d, 0xda, 0x65, 0x5d, 0x68, 0xf6, 0x4b,
+	0xb3, 0x5b, 0x69, 0xaf, 0x3e, 0x81, 0x20, 0x3e, 0x8b, 0xaf, 0xd0, 0x63, 0xc1, 0x8b, 0x27, 0x91,
+	0xd6, 0x07, 0x91, 0xee, 0x6e, 0x22, 0xf1, 0x8f, 0xdc, 0xbe, 0xcc, 0xce, 0x7c, 0x33, 0xdf, 0x10,
+	0xd4, 0xe4, 0x42, 0xf1, 0x21, 0xa3, 0x02, 0x8f, 0x27, 0x34, 0x9f, 0xc5, 0x59, 0x0e, 0x0a, 0xfc,
+	0x60, 0x9a, 0x82, 0x20, 0x43, 0x9a, 0xc7, 0xc5, 0x73, 0x39, 0x04, 0x4d, 0x06, 0x0c, 0x34, 0x0d,
+	0xaf, 0x27, 0xa3, 0x08, 0x0e, 0x18, 0x00, 0x1b, 0x51, 0x4c, 0x32, 0x8e, 0x89, 0x10, 0xa0, 0x88,
+	0xe2, 0x20, 0xa4, 0x7d, 0x6d, 0x0d, 0x40, 0xa6, 0x20, 0x71, 0x42, 0x24, 0x35, 0x46, 0xf8, 0xae,
+	0x93, 0x50, 0x45, 0x3a, 0x38, 0x23, 0x8c, 0x0b, 0x4d, 0xb6, 0xdc, 0xdd, 0x32, 0x51, 0x46, 0x72,
+	0x92, 0xda, 0x15, 0x51, 0x13, 0xf9, 0x57, 0x6b, 0xe1, 0xa5, 0x06, 0xfb, 0x74, 0x3c, 0xa1, 0x52,
+	0x45, 0xd7, 0x68, 0xa7, 0x82, 0xca, 0x0c, 0x84, 0xa4, 0xfe, 0x19, 0xf2, 0x8c, 0x78, 0xdf, 0x3d,
+	0x72, 0x4f, 0xb6, 0xbb, 0x51, 0xfc, 0xf7, 0x41, 0xb1, 0xd1, 0xf6, 0x36, 0xe6, 0x6f, 0x87, 0x4e,
+	0xdf, 0xea, 0x4a, 0xbb, 0x0b, 0x22, 0x15, 0x1f, 0x16, 0x76, 0x6d, 0x6b, 0x57, 0xa0, 0xd6, 0x6e,
+	0x0f, 0x79, 0x23, 0x8d, 0x68, 0xbb, 0xad, 0xbe, 0xfd, 0xea, 0x3e, 0x37, 0xd0, 0xa6, 0xe6, 0xfb,
+	0x4f, 0x2e, 0xf2, 0x8c, 0x8f, 0x1f, 0xff, 0x97, 0xe5, 0xe7, 0x89, 0x01, 0xae, 0xcd, 0x37, 0x69,
+	0xa2, 0xd6, 0xfd, 0xcb, 0xc7, 0x63, 0xe3, 0xd8, 0x8f, 0x70, 0x21, 0xc4, 0x65, 0xa5, 0xdf, 0xba,
+	0xd5, 0xb9, 0xcc, 0x31, 0x35, 0x72, 0x55, 0xba, 0xa8, 0x91, 0xab, 0xda, 0x52, 0xbd, 0x5c, 0xa6,
+	0xb9, 0xde, 0xf9, 0x7c, 0x19, 0xba, 0x8b, 0x65, 0xe8, 0xbe, 0x2f, 0x43, 0xf7, 0x61, 0x15, 0x3a,
+	0x8b, 0x55, 0xe8, 0xbc, 0xae, 0x42, 0xe7, 0xa6, 0xcd, 0xb8, 0xba, 0x9d, 0x24, 0xf1, 0x00, 0xd2,
+	0x5f, 0xf6, 0x4c, 0xbf, 0x46, 0x35, 0xcb, 0xa8, 0x4c, 0x3c, 0xfd, 0xf7, 0x9c, 0x7e, 0x06, 0x00,
+	0x00, 0xff, 0xff, 0x3e, 0xcf, 0x93, 0xdd, 0xe8, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -157,6 +242,8 @@ const _ = grpc.SupportPackageIsVersion4
 type QueryClient interface {
 	// Parameters queries the parameters of the module.
 	Params(ctx context.Context, in *QueryParamsRequest, opts ...grpc.CallOption) (*QueryParamsResponse, error)
+	// Queries a list of Lastid items.
+	Lastid(ctx context.Context, in *QueryLastidRequest, opts ...grpc.CallOption) (*QueryLastidResponse, error)
 }
 
 type queryClient struct {
@@ -176,10 +263,21 @@ func (c *queryClient) Params(ctx context.Context, in *QueryParamsRequest, opts .
 	return out, nil
 }
 
+func (c *queryClient) Lastid(ctx context.Context, in *QueryLastidRequest, opts ...grpc.CallOption) (*QueryLastidResponse, error) {
+	out := new(QueryLastidResponse)
+	err := c.cc.Invoke(ctx, "/xmonader.intidgen.intidgen.Query/Lastid", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // QueryServer is the server API for Query service.
 type QueryServer interface {
 	// Parameters queries the parameters of the module.
 	Params(context.Context, *QueryParamsRequest) (*QueryParamsResponse, error)
+	// Queries a list of Lastid items.
+	Lastid(context.Context, *QueryLastidRequest) (*QueryLastidResponse, error)
 }
 
 // UnimplementedQueryServer can be embedded to have forward compatible implementations.
@@ -188,6 +286,9 @@ type UnimplementedQueryServer struct {
 
 func (*UnimplementedQueryServer) Params(ctx context.Context, req *QueryParamsRequest) (*QueryParamsResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Params not implemented")
+}
+func (*UnimplementedQueryServer) Lastid(ctx context.Context, req *QueryLastidRequest) (*QueryLastidResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Lastid not implemented")
 }
 
 func RegisterQueryServer(s grpc1.Server, srv QueryServer) {
@@ -212,6 +313,24 @@ func _Query_Params_Handler(srv interface{}, ctx context.Context, dec func(interf
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Query_Lastid_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(QueryLastidRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(QueryServer).Lastid(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/xmonader.intidgen.intidgen.Query/Lastid",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(QueryServer).Lastid(ctx, req.(*QueryLastidRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Query_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "xmonader.intidgen.intidgen.Query",
 	HandlerType: (*QueryServer)(nil),
@@ -219,6 +338,10 @@ var _Query_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Params",
 			Handler:    _Query_Params_Handler,
+		},
+		{
+			MethodName: "Lastid",
+			Handler:    _Query_Lastid_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -281,6 +404,59 @@ func (m *QueryParamsResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	return len(dAtA) - i, nil
 }
 
+func (m *QueryLastidRequest) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryLastidRequest) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryLastidRequest) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
+func (m *QueryLastidResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *QueryLastidResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *QueryLastidResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Lastid) > 0 {
+		i -= len(m.Lastid)
+		copy(dAtA[i:], m.Lastid)
+		i = encodeVarintQuery(dAtA, i, uint64(len(m.Lastid)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintQuery(dAtA []byte, offset int, v uint64) int {
 	offset -= sovQuery(v)
 	base := offset
@@ -309,6 +485,28 @@ func (m *QueryParamsResponse) Size() (n int) {
 	_ = l
 	l = m.Params.Size()
 	n += 1 + l + sovQuery(uint64(l))
+	return n
+}
+
+func (m *QueryLastidRequest) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *QueryLastidResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Lastid)
+	if l > 0 {
+		n += 1 + l + sovQuery(uint64(l))
+	}
 	return n
 }
 
@@ -429,6 +627,138 @@ func (m *QueryParamsResponse) Unmarshal(dAtA []byte) error {
 			if err := m.Params.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryLastidRequest) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryLastidRequest: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryLastidRequest: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipQuery(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *QueryLastidResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowQuery
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: QueryLastidResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: QueryLastidResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Lastid", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowQuery
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthQuery
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthQuery
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Lastid = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
